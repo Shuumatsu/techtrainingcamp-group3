3366951e2d2de02d895cef2afa6d12af7dbba97b
Akatsuki <VanishingDante@gmail.com>
2021-10-31 17:34:25 +0800

    fix typo

--- main.go
+++ main.go
@@ -7,5 +7,5 @@ import (
 func main() {
 	r := router.Register()
 
-	r.Run(":80") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run("0.0.0.0:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
