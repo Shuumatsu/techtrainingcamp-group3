0123f1208c429fb32fd9e2373639e595f97bff50
ltlin9 <2741418638@qq.com>
2021-10-31 17:02:46 +0800

    add middlewares

--- api/router/router.go
+++ api/router/router.go
@@ -3,14 +3,14 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"techtrainingcamp-group3/api/handler"
+	"techtrainingcamp-group3/tools/middlewares"
 )
 
 func Register() *gin.Engine {
 	r := gin.Default()
-
+	r.Use(middlewares.Cors())
 	r.POST("/snatch", handler.SnatchHandler)
 	r.POST("/open", handler.OpenHandler)
 	r.POST("/get_wallet_list", handler.WalletListHandler)
-
 	return r
 }
