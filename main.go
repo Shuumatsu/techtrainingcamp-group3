8e43ac1427985fd8bdaafb63be2ad9def53fd386
ltlin9 <2741418638@qq.com>
2021-10-31 17:03:00 +0800

    listen and serve :80

--- main.go
+++ main.go
@@ -7,6 +7,6 @@ import (
 func main() {
 	r := router.Register()
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":80") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
