194537162248723add6d0d3b5cbebf7afafb6466
ltlin9 <2741418638@qq.com>
2021-10-31 17:18:03 +0800

    reconstruct project

--- api/router/router.go
+++ api/router/router.go
@@ -3,7 +3,7 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"techtrainingcamp-group3/api/handler"
-	"techtrainingcamp-group3/tools/middlewares"
+	"techtrainingcamp-group3/api/middlewares"
 )
 
 func Register() *gin.Engine {
