3bf162c0d3c0986f7b72d22902da449892c06a22
ltlin9 <2741418638@qq.com>
2021-11-02 17:10:17 +0800

    test database tables

--- db/database_builder.sql
+++ db/database_builder.sql
@@ -10,9 +10,7 @@ CREATE TABLE user(
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(uid)
 );
-INSERT INTO user(`uid`, `envelope_list`) VALUES (1, "1,2,3");
-INSERT INTO user(`uid`, `envelope_list`) VALUES (2, "4");
-INSERT INTO user(`uid`, `envelope_list`) VALUES (3, "5,6");
+
 CREATE TABLE user_envelope(
     uid BIGINT NOT NULL,
     envelope_id BIGINT NOT NULL
@@ -25,12 +23,7 @@ CREATE TABLE envelope(
     snatched_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(envelope_id)
 );
-INSERT INTO envelope(`envelope_id`, `open_stat`, `value`) values (1, 1, 50);
-INSERT INTO envelope(`envelope_id`, `open_stat`, `value`) values (2, 0, 515130);
-INSERT INTO envelope(`envelope_id`, `open_stat`, `value`) values (3, 0, 5460);
-INSERT INTO envelope(`envelope_id`, `open_stat`, `value`) values (4, 1, 5044566);
-INSERT INTO envelope(`envelope_id`, `open_stat`, `value`) values (5, 0, 54560);
-INSERT INTO envelope(`envelope_id`, `open_stat`, `value`) values (6, 1, 7897950);
+
 /*Future table below*/
 /*
 CREATE TABLE pool_list(
