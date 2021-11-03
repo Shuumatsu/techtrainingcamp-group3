31fe93dc2a4b3dc8d5aa66344f8bc7219af076f0
ltlin9 <2741418638@qq.com>
2021-11-03 17:15:51 +0800

    test mongodb

--- db/mongo/mongodb_test.go
+++ db/mongo/mongodb_test.go
@@ -5,8 +5,132 @@ import (
 	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"techtrainingcamp-group3/models"
 	"testing"
+	"time"
 )
+
+type userTest struct {
+	UID      int64 `bson:"uid"`
+	CurCount int64 `bson:"cur_count"`
+	models.WalletListData
+}
+
+func TestEnvelope(t *testing.T) {
+	collection := MG.Collection("userTest")
+	user1 := userTest{
+		UID:      1,
+		CurCount: 2,
+		WalletListData: models.WalletListData{
+			Amount: 10086,
+			EnvelopeList: []models.Envelope{
+				{
+					EnvelopeId: 1,
+					Opened:     false,
+					Value:      6546,
+					SnatchTime: time.Now().Unix(),
+				},
+				{
+					EnvelopeId: 2,
+					Opened:     false,
+					Value:      56161,
+					SnatchTime: time.Now().Unix(),
+				},
+			},
+		},
+	}
+	user2 := userTest{
+		UID:      2,
+		CurCount: 1,
+		WalletListData: models.WalletListData{
+			Amount: 15446,
+			EnvelopeList: []models.Envelope{
+				{
+					EnvelopeId: 3,
+					Opened:     false,
+					Value:      654456,
+					SnatchTime: time.Now().Unix(),
+				},
+			},
+		},
+	}
+	user3 := userTest{
+		UID:      3,
+		CurCount: 0,
+		WalletListData: models.WalletListData{
+			Amount:       10086,
+			EnvelopeList: nil,
+		},
+	}
+	users := []interface{}{user1, user2, user3}
+	insertResult, err := collection.InsertMany(context.TODO(), users)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println("Inserted userTest: ", insertResult.InsertedIDs)
+	newEnvelopeList := []models.Envelope{
+		{
+			EnvelopeId: 4,
+			Opened:     false,
+			Value:      754456,
+			SnatchTime: time.Now().Unix(),
+		},
+		{
+			EnvelopeId: 5,
+			Opened:     false,
+			Value:      653356,
+			SnatchTime: time.Now().Unix(),
+		},
+	}
+	filter := bson.D{{"uid", 3}}
+	update := bson.D{
+		{"$inc", bson.D{
+			{"cur_count", 2},
+		}},
+		{"$set", bson.D{
+			{"WalletListData.EnvelopeList", newEnvelopeList},
+		}},
+	}
+	updateResult, err := collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("Matched %v documents and updated %v documents.\n",
+		updateResult.MatchedCount,
+		updateResult.ModifiedCount)
+	// Here's an array in which you can store the decoded documents
+	var results []*userTest
+	// Passing bson.D{{}} as the filter matches all documents in the collection
+	cur, err := collection.Find(context.TODO(), bson.D{{}}, options.Find())
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Finding multiple documents returns a cursor
+	// Iterating through the cursor allows us to decode documents one at a time
+	for cur.Next(context.TODO()) {
+		// create a value into which the single document can be decoded
+		var elem userTest
+		err := cur.Decode(&elem)
+		if err != nil {
+			t.Fatal(err)
+		}
+		results = append(results, &elem)
+	}
+	if err := cur.Err(); err != nil {
+		t.Fatal(err)
+	}
+	// Close the cursor once finished
+	cur.Close(context.TODO())
+	fmt.Printf("Found multiple documents (array of pointers): %+v\n", results)
+	for _, u := range results {
+		fmt.Println(*u)
+	}
+	deleteResult, err := collection.DeleteMany(context.TODO(), bson.D{{}})
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("Deleted %v documents in the trainers collection\n", deleteResult.DeletedCount)
+}
 
 type trainer struct {
 	Name string
