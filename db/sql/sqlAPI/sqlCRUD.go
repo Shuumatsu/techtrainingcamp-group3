d880932bfd5d191566f5dc01794f38123f596570
ltlin9 <2741418638@qq.com>
2021-11-05 17:13:17 +0800

    add sql api CRUD

--- db/sql/sqlAPI/sqlCRUD.go
+++ db/sql/sqlAPI/sqlCRUD.go
@@ -2,14 +2,30 @@ package sqlAPI
 
 import (
 	"techtrainingcamp-group3/db/dbmodels"
+	"techtrainingcamp-group3/db/sql"
+	"techtrainingcamp-group3/logger"
 )
 
-func FindOrCreateUserByUID(uid dbmodels.UID) (dbmodels.User, error) {
-	return dbmodels.User{}, Error.FuncNotDefined
+func FindOrCreateUserByUID(uid dbmodels.UID) (*dbmodels.User, error) {
+	var user dbmodels.User
+	if err := sql.DB.
+		Table(dbmodels.User{}.TableName()).
+		FirstOrCreate(&user, uid).Error; err != nil {
+		logger.Sugar.Debugw("FindOrCreateUserByUID", "error", err)
+		return nil, err
+	}
+	return &user, nil
 }
 
-func FindUserByUID(uid dbmodels.UID) (dbmodels.User, error) {
-	return dbmodels.User{}, Error.FuncNotDefined
+func FindUserByUID(uid dbmodels.UID) (*dbmodels.User, error) {
+	var user dbmodels.User
+	if err := sql.DB.
+		Table(dbmodels.User{}.TableName()).
+		First(&user, uid).Error; err != nil {
+		logger.Sugar.Debugw("FindUserByUID", "error", err)
+		return nil, err
+	}
+	return &user, nil
 }
 
 func FindEnvelopesByUID(uid dbmodels.UID) ([]dbmodels.Envelope, error) {
@@ -20,8 +36,15 @@ func AddEnvelopeToUserByUID(uid dbmodels.UID, envelope dbmodels.Envelope) error 
 	return Error.FuncNotDefined
 }
 
-func FindEnvelopeByEID(eid dbmodels.EID) (dbmodels.Envelope, error) {
-	return dbmodels.Envelope{}, Error.FuncNotDefined
+func FindEnvelopeByEID(eid dbmodels.EID) (*dbmodels.Envelope, error) {
+	var envelope dbmodels.Envelope
+	if err := sql.DB.
+		Table(dbmodels.Envelope{}.TableName()).
+		First(&envelope, eid).Error; err != nil {
+		logger.Sugar.Debugw("FindEnvelopeByEID", "error", err)
+		return nil, err
+	}
+	return &envelope, nil
 }
 
 func OpenEnvelopeByEID(eid dbmodels.EID) error {
